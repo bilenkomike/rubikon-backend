854e5d13e9242e8a05611131aff520d42285640c
mike <mike@cdontiy.com>
2026-01-29 15:09:02 +0200

    adjusted the settings

--- core/settings.py
+++ core/settings.py
@@ -139,7 +139,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
@@ -173,5 +174,3 @@ if not DEBUG:
 	SECURE_HSTS_SECONDS = 31536000
 	SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 	SECURE_HSTS_PRELOAD = True
-	STATIC_URL = "/static/"
-	STATIC_ROOT = BASE_DIR / "staticfiles"
