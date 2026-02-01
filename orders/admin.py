c0aa49a11b73af88c2ae4ebe03f3e135c73d0b59
mike <mike@cdontiy.com>
2026-02-01 11:17:26 +0200

    adjusted the things

--- orders/admin.py
+++ orders/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Order, OrderItem, Wishlist
+from .models import Order, OrderItem, Wishlist, CartItem
 
 
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(Wishlist)
+admin.site.register(CartItem)
+
